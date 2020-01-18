979c62fbfe68b31fc46364c52b32d83344b26830
EC2 Default User <ec2-user@ip-172-31-28-160.us-west-2.compute.internal>
2020-01-18 22:36:54 +0000

    added unit tests to the two sorts

---
+++ test_sortalgorithm.py
@@ -0,0 +1,12 @@
+import unittest
+from sortalgorithm import SortAlgorithm
+
+class TestSort(unittest.TestCase):
+    def test_BubbleSort(self):
+        SortObj = SortAlgorithm()
+        result = SortObj.BubbleSort([10,7,8,4,3,6,9,5,1,2])
+        self.assertSequenceEqual(result,[1,2,3,4,5,6,7,8,9,10])
+    def test_SelectSort(self):
+        SortObj = SortAlgorithm()
+        result = SortObj.SelectSort([10,7,8,4,3,6,9,5,1,2])
+        self.assertSequenceEqual(result,[1,2,3,4,5,6,7,8,9,10])
