d329816968c17aea5bef66d2f1e05de8a13f8c83
jagadhesh89 <jagadhesh89@gmail.com>
2020-04-28 09:23:55 -0500

    Update test_sortalgorithm.py

--- test_sortalgorithm.py
+++ test_sortalgorithm.py
@@ -5,7 +5,7 @@ class TestSort(unittest.TestCase):
     def test_BubbleSort(self):
         SortObj = SortAlgorithm()
         result = SortObj.BubbleSort([10,7,8,4,3,6,9,5,1,2])
-        self.assertSequenceEqual(result,[1,2,3,4,5,6,7,8,9])
+        self.assertSequenceEqual(result,[1,2,3,4,5,6,7,8])
     def test_SelectSort(self):
         SortObj = SortAlgorithm()
         result = SortObj.SelectSort([10,7,8,4,3,6,9,5,1,2])
